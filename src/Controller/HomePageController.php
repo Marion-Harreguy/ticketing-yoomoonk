9f9ef1eb07df08e59794ab12a766f202f71e8f08
Marion Harreguy <marionharreguy@gmail.com>
2023-01-19 09:34:26 +0100

    [features] add Customer entity + HP controller + css + route homepage

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/", name="homepage_")
+*/
+class HomePageController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('homepage/index.html.twig');
+    }
+}
